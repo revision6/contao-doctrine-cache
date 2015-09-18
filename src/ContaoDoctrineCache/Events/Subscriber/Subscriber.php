a0d083a3f4c626d8fdff3023adeb0f95df5fa2de
cboelter <christopher@boelter.eu>
2015-09-18 17:17:18 +0200

    removed comments

--- src/ContaoDoctrineCache/Events/Subscriber/Subscriber.php
+++ src/ContaoDoctrineCache/Events/Subscriber/Subscriber.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * The Postmanager allows you to manage posts and news in a from the contao core separated system
- *
- * PHP version 5
- *
- * @package    PostManager
- * @subpackage PostManager
- * @author     Christopher Boelter <c.boelter@revision6.de>
- * @copyright  Revision6 UG
- * @license    LGPL.
- * @filesource
- */
 
 namespace ContaoDoctrineCache\Events\Subscriber;
 
@@ -19,11 +7,6 @@ use ContaoDoctrineCache\Events\Event\FlushCacheEvent;
 use ContaoDoctrineCache\Events\Event\RebuildCacheEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-/**
- * The BaseSubscriber for extended all other subscriber.
- *
- * @package PostManager\DataContainer\Events\Table
- */
 class Subscriber implements EventSubscriberInterface
 {
 
